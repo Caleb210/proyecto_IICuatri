b33c280308aa02d4e88bd18170158bc63657a370
Caleb210 <148110541+Caleb210@users.noreply.github.com>
2024-07-03 20:13:44 -0600

    agregue el import

--- src/main/java/com/mycompany/proyecto_restaurante/Proyecto_Restaurante.java
+++ src/main/java/com/mycompany/proyecto_restaurante/Proyecto_Restaurante.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.proyecto_restaurante;
 
-/**
- *
- * @author bisme
- */
+import javax.swing.JOptionPane;
 public class Proyecto_Restaurante {
 
     public static void main(String[] args) {
