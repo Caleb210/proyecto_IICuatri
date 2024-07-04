581f1ebeb04f9be6af3a3fbd8267ea0a509d96e9
Caleb210 <148110541+Caleb210@users.noreply.github.com>
2024-07-03 20:31:24 -0600

    otro cambio

--- src/main/java/com/mycompany/proyecto_restaurante/Proyecto_Restaurante.java
+++ src/main/java/com/mycompany/proyecto_restaurante/Proyecto_Restaurante.java
@@ -5,6 +5,6 @@ import javax.swing.JOptionPane; //importacion
 public class Proyecto_Restaurante {
 
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("otro");
     }
 }
