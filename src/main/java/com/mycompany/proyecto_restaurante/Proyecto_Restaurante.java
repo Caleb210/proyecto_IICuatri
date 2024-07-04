4aa980236a573f1b9183bc9d3d884ea34f1d9949
Caleb210 <148110541+Caleb210@users.noreply.github.com>
2024-07-03 20:01:50 -0600

    init commit

---
+++ src/main/java/com/mycompany/proyecto_restaurante/Proyecto_Restaurante.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_restaurante;
+
+/**
+ *
+ * @author bisme
+ */
+public class Proyecto_Restaurante {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
